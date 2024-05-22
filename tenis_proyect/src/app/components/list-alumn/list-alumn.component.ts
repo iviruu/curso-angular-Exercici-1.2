e0a65476784b03ee74942bd90f1e3b8cda0b82ac
iviruu <ivan.kok.ru@gmail.com>
2024-05-23 00:49:23 +0200

    chore: update angular.json and app.routes.ts

---
+++ tenis_proyect/src/app/components/list-alumn/list-alumn.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-alumn',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-alumn.component.html',
+  styleUrl: './list-alumn.component.css'
+})
+export class ListAlumnComponent {
+
+}
