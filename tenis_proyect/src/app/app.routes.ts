ed146f69e5a1d1d5fd10b2cc821f9a5c60bdb7a7
iviruu <ivan.kok.ru@gmail.com>
2024-05-23 13:23:03 +0200

    chore: Update app.component.ts and app.routes.ts

--- tenis_proyect/src/app/app.routes.ts
+++ tenis_proyect/src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { AlumnoComponent } from './alumno/alumno.component';
 
 export const routes: Routes = [
-    
+    {path: 'home', component: HomeComponent},
+    {path: 'teacher', component: TeacherComponent},
+    {path: 'alumno', component: AlumnoComponent},
+    {path: '**', redirectTo: 'home', pathMatch: 'full'}
+
+
 ];
